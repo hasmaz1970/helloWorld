4e594ac9ceede14474fb14cade939d272f431ca2
Hudai Sami Asmaz <hudai.asmaz@pia-systems.com>
2025-07-30 22:20:31 -0700

    .

--- docs/javascripts/external-links.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("a[href^='http']").forEach(function (el) {
-    el.setAttribute("target", "_blank");
-    el.setAttribute("rel", "noopener");
-  });
-});
